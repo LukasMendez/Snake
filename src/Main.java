8f75546d554aeeacc6f08a19be666b5a9c249b14
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2019-04-29 12:41:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Lukas
+ * 29-04-2019.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+
+
+
+
+
+
+
+
+
+    }
+}
