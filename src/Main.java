1bc3bc5f0176bff47899df1d871cd64c3db93dae
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2019-04-30 11:46:10 +0200

    - It fucking works, and sometimes not

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,7 @@ public class Main extends Application {
     BorderPane root = new BorderPane();
     Canvas canvas = new Canvas(700, 700);
     Snake snake = new Snake();
+    Food food = new Food();
     private GraphicsContext gc = canvas.getGraphicsContext2D();
     private SimpleStringProperty currentDirection = new SimpleStringProperty();
     private boolean gameOver = false;
@@ -61,7 +62,21 @@ public class Main extends Application {
                     if (!gameOver) {
 
                         clearCanvas();
+                        drawFood();
                         drawSnake();
+
+                        if(snake.getHead().getxCord() == food.getFood().getxCord() && snake.getHead().getyCord() == food.getFood().getyCord()){
+                            snake.addHead(currentDirection);
+                            food.refreshFood();
+                        }
+                        else{
+                            snake.addHead(currentDirection);
+                            snake.removeTail();
+                        }
+
+
+
+
                         checkIfEatingItself();
                         snake.addHead(currentDirection);
                         snake.removeTail();
@@ -220,6 +235,12 @@ public class Main extends Application {
     private void clearCanvas() {
         gc.clearRect(0, 0, 700, 700);
     }
+
+    private void drawFood(){
+        gc = canvas.getGraphicsContext2D();
+        canvas.getGraphicsContext2D().fillOval(food.getFood().getxCord(), food.getFood().getyCord(), 10, 10);
+    }
+
 
     private void drawGameOver() {
 
